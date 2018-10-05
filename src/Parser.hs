42bee2259fe56e6727a537a7afe5a30b68278252
eddie <eudes.scar@gmail.com>
2018-10-05 03:32:25 -0300

    Symbols, atoms and strings parsers

--- src/Parser.hs
+++ src/Parser.hs
@@ -8,3 +8,24 @@ data LipsVal = LipsNumber Integer
              | LipsBool Bool
              | LipsAtom Atom
              deriving (Eq, Show)
+
+lipsSymbol :: Parser Char
+lipsSymbol = oneOf "!#$%&|*+-/:><?=@^_~"
+
+lipsAtom :: Parser LipsVal
+lipsAtom = do
+  first <- letter <|> lipsSymbol
+  rest  <- many (letter <|> digit <|> lipsSymbol)
+  let atom = first : rest
+  pure $
+    case atom of
+      "#t" -> LipsBool True
+      "#f" -> LipsBool False
+      _    -> LipsAtom atom
+
+lipsString :: Parser LipsVal
+lipsString = do
+  char '"'
+  str <- many (noneOf "\"")
+  char '"'
+  pure (LipsString str)

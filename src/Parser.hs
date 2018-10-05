4dd9e26db41318eba802cf5d1af016af7d959b40
eddie <eudes.scar@gmail.com>
2018-10-05 03:31:57 -0300

    LIPS structure

--- src/Parser.hs
+++ src/Parser.hs
@@ -2,3 +2,9 @@ module Parser where
 
 import Control.Applicative
 import Text.Trifecta
+
+data LipsVal = LipsNumber Integer
+             | LipsString String
+             | LipsBool Bool
+             | LipsAtom Atom
+             deriving (Eq, Show)

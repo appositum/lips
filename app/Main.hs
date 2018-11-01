35b731d14110656b9ae341cf06a8fa40620eec32
eddie <appositum@protonmail.com>
2018-11-01 02:06:34 -0300

    Basic loop

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,25 @@
 module Main where
 
+import Control.Monad (unless)
 import Parser
 import System.Environment
+import System.IO
 
 main :: IO ()
 main = do
   arg <- getArgs
   case arg of
-    [] -> putStrLn "lips: no input file"
+    [] -> repl
     (path:_) -> readFile path >>= print . readExpr
+
+replRead :: IO String
+replRead = do
+  putStr "LIPS> "
+  hFlush stdout
+  getLine
+
+repl :: IO ()
+repl = do
+  input <- replRead
+  unless (input == ":q" || input == ":quit") $
+    putStrLn input *> main

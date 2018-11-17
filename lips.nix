ed4cdb47097d26cdb4c26e3c7dfb51f2e7d5ef2a
eddie <appositum@protonmail.com>
2018-11-17 00:37:28 -0300

    Megaparsec on nix

--- lips.nix
+++ lips.nix
@@ -1,12 +1,12 @@
-{ mkDerivation, base, megaparsec_7_0_4, parsers-megaparsec, stdenv }:
+{ mkDerivation, base, megaparsec, parsers-megaparsec, stdenv }:
 mkDerivation {
   pname = "lips";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
-  libraryHaskellDepends = [ base megaparsec_7_0_4 parsers-megaparsec ];
-  executableHaskellDepends = [ base megaparsec_7_0_4 parsers-megaparsec ];
+  libraryHaskellDepends = [ base megaparsec parsers-megaparsec ];
+  executableHaskellDepends = [ base megaparsec parsers-megaparsec ];
   testHaskellDepends = [ base ];
   homepage = "https://github.com/appositum/lips#readme";
   license = stdenv.lib.licenses.asl20;

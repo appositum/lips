36d641466aa666563b126359b4c2e8dc845e8a70
eddie <appositum@protonmail.com>
2018-11-19 16:20:04 -0300

    Update nix build (again)

--- shell.nix
+++ shell.nix
@@ -3,13 +3,12 @@
 let
   inherit (nixpkgs) pkgs;
 
-  haskellPackages = if compiler == "default"
-                       then pkgs.haskellPackages
-                       else pkgs.haskell.packages.${compiler};
+  haskellPackages =
+    if compiler == "default"
+    then pkgs.haskellPackages
+    else pkgs.haskell.packages.${compiler};
 
   variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
-
   drv = variant (haskellPackages.callPackage ./default.nix {});
-
 in
   if pkgs.lib.inNixShell then drv.env else drv
